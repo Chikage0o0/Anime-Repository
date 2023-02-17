ea7c68337b2f5b1f5ef4607c8d216837d3561b47
Chikage <chikage@939.me>
2023-02-18 00:18:30 +0800

    prepare refactor frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { ConfigProvider, Layout, theme } from 'antd'
-import SiderMenu from '@/components/menu/sider'
+import Layout from '@/layout'
 import { useRoutes } from 'react-router-dom'
 import routes from '@/router'
 import { observer } from 'mobx-react-lite'
 import { get_antd_locale } from '@/locales/locales'
 import setting, { SettingObject } from '@/store/settingStore'
 import { getTheme } from '@/theme'
-const { defaultAlgorithm, darkAlgorithm } = theme
 
 function App() {
-  const outlet = useRoutes(routes)
-  const theme = getTheme(setting.setting as SettingObject)
+  // const outlet = useRoutes(routes)
+  // const theme = getTheme(setting.setting as SettingObject)
 
   return (
     <div className="container">
-      <ConfigProvider
+      {/* <ConfigProvider
         locale={get_antd_locale(setting.setting as SettingObject)}
         theme={{
           algorithm: theme,
@@ -28,7 +26,8 @@ function App() {
           <SiderMenu />
           <Layout>{outlet}</Layout>
         </Layout>
-      </ConfigProvider>
+      </ConfigProvider> */}
+      <Layout />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 //样式初始化
 import 'reset-css'
 //全局样式
@@ -10,6 +9,7 @@ import setting, { SettingObject } from './store/settingStore'
 import { initReactI18next } from 'react-i18next'
 import { resources } from './locales/locales'
 import i18n from 'i18next'
+import ReactDOM from 'react-dom'
 
 // import { listen } from '@tauri-apps/api/event'
 
@@ -19,31 +19,32 @@ document.addEventListener('contextmenu', function (e) {
 })
 
 //初始化配置信息
-await setting.init()
-i18n
-  // 将 i18n 实例传递给 react-i18next
-  .use(initReactI18next)
-  // 初始化 i18next
-  // 所有配置选项: https://www.i18next.com/overview/configuration-options
-  .init({
-    resources,
-    fallbackLng: 'en_US',
-    lng: (setting.setting as SettingObject).ui.lang.replace(/-/, '_'),
-    debug: false,
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-  })
+// await setting.init()
+// i18n
+//   // 将 i18n 实例传递给 react-i18next
+//   .use(initReactI18next)
+//   // 初始化 i18next
+//   // 所有配置选项: https://www.i18next.com/overview/configuration-options
+//   .init({
+//     resources,
+//     fallbackLng: 'en_US',
+//     lng: (setting.setting as SettingObject).ui.lang.replace(/-/, '_'),
+//     debug: false,
+//     interpolation: {
+//       escapeValue: false, // not needed for react as it escapes by default
+//     },
+//   })
 //监听后端错误信息
 // await listen<Object>('get_setting', (event) => {
 //   console.log(event.payload)
 // })
 // invoke('get_setting')
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
 )
